d4fcae5e1795daf8972aab697a20629df78aa564
Pandelis Zembashis <pandelis@d4software.co.uk>
2017-12-02 16:37:44 +0000

    Image processing

--- public/seefood.js
+++ public/seefood.js
@@ -7,10 +7,83 @@ $( document ).ready(function() {
 
   })
 
-
-  $('input[type="file"]').change(function() {
-    if ($(this).val()) {
-      console.log('uploaded')
+  function showImagePreview(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader()
+    
+      reader.onload = function(e) {
+        $('#preview').attr('src', e.target.result)
+      }
+    
+      reader.readAsDataURL(input.files[0])
     }
+  }
+
+  var imageContains = function(clarifai, foodType, threshold) {
+    var concepts = clarifai.outputs[0].data.concepts
+  
+    console.log(concepts)
+  
+    concepts.forEach(element => {
+      if (element.name === foodType && element.value >= threshold) {
+        return true
+      }
+    })
+  
+    return false
+  }
+
+  $('input[type="file"]').change(function (event) {
+    var files = event.target.files
+    $('.preview').show()
+    $('.touchToSee').hide()
+    showImagePreview(this)
+
+    event.stopPropagation() // Stop stuff happening
+    event.preventDefault() // Totally stop stuff happening
+
+    // START A LOADING SPINNER HERE
+
+    // Create a formdata object and add the files
+    var data = new FormData()
+    data.append('food', files[0])
+
+
+
+    $.ajax({
+      url: 'submit',
+      type: 'POST',
+      data: data,
+      cache: false,
+      dataType: 'json',
+      processData: false, // Don't process the files
+      contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+      success: function(data) {
+        if(data.status === 'success') {
+          
+          var containsFoodItem = imageContains(data.clarifai, 'sandwich', 0.90)
+
+          if (containsFoodItem == true) {
+
+            $('.tick').show()
+
+          } else {
+            $('.cross').show()
+          }
+        }
+        else {
+          // Handle errors here
+          console.log('ERRORS: ' + data.message)
+        }
+      },
+      error: function(err) {
+        // Handle errors here
+        console.log('ERRORS: ' + err)
+        // STOP LOADING SPINNER
+      }
+    })
   })
+
+  // Grab the files and set them to our variable
+  
 })
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express')
 const app = express()
+const multer  = require('multer')
+require('dotenv').config()
+const upload = multer({ storage: multer.memoryStorage({}) })
+const Clarifai = require('clarifai')
+
+const clarifai = new Clarifai.App({
+  apiKey: process.env.CLARIFAI
+})
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -14,6 +18,47 @@ app.use(express.static('public'))
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html')
+})
+
+
+const imageContains = function(clarifai, foodType, threshold) {
+  const concepts = clarifai.outputs.data.concepts
+
+  console.log(concepts)
+
+  concepts.forEach(element => {
+    if (element.name === foodType && element.value >= threshold) {
+      return true
+    }
+  })
+
+  return false
+}
+
+
+app.post('/submit', upload.single('food'), function(request, response) {
+  
+  const base64Image = request.file.buffer.toString('base64')
+
+  //Clarifai process the image requestnpm install 
+  clarifai.models.predict(Clarifai.FOOD_MODEL, {base64: base64Image}).then(
+    function(res) {
+
+      response.send({
+        status: 'success',
+        clarifai: res
+      })
+    },
+    function(err) {
+      response.send({status: 'error', message: err})
+    }
+  ).catch(function(err) {
+    response.send({status: 'error', message: err})
+  })
+
+  
+  
+  
 })
 
 
